528bb832de42811f3ed96fe70751d15a5bc25735
Gabriel Costa <gabriel.costa@airy3d.com>
2021-03-18 15:15:12 -0400

    SocketUDP Class and examples have been added

--- examples/SocketUDPClient/main.cpp
+++ examples/SocketUDPClient/main.cpp
@@ -1,21 +1,21 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
-#include "SocketTCPClient.hpp"
+#include "SocketUDP.hpp"
 
 using namespace std;
 
 int main(int argc, char** argv) {
-    SocketTCP::STATUS status = SocketTCP::CLOSE;
-    SocketTCP::Client client;
+    bool status = false;
+    UDP client;
 
     string message_out = "Hello World!";
     uint8_t message_in[32] = {0};
     string message;
     int32_t bytes = 0;
 
-    status = client.Open("127.0.0.1", 5000);
-    if (status == SocketTCP::STATUS::OPEN){
+    status = client.Open(CLIENT, "127.0.0.1", 5000, true);
+    if (status == true){
         status = client.Write(message_out.c_str() , message_out.size(), &bytes);
         status = client.Read(message_in, sizeof(message_in), &bytes);
         cout << "Client:" << message_in << endl;
